c673e432af30ea1f6c6aeeff16e45dba54111098
Bill Little <bill.james.little@gmail.com>
2018-03-06 15:17:19 +0000

    bump to 2.2.0dev0

--- iris_sample_data/__init__.py
+++ iris_sample_data/__init__.py
@@ -4,7 +4,7 @@ import os
 
 
 # Iris sample data version.
-__version__ = '2.1.0'
+__version__ = '2.2.0dev0'
 
 # Root directory containing the iris sample data.
 path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sample_data')
