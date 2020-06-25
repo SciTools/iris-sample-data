c1290a046a227732af9e9b24989d93e9714beced
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-06-25 14:29:59 +0100

    Bump version for start of 2.4.0 development.

--- iris_sample_data/__init__.py
+++ iris_sample_data/__init__.py
@@ -4,7 +4,7 @@ import os
 
 
 # Iris sample data version.
-__version__ = '2.3.0'
+__version__ = '2.4.0dev'
 
 # Root directory containing the iris sample data.
 path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sample_data')
